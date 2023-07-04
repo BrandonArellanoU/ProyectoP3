86dcb25465e82f1172af09eff86955d1d71c2756
Lyriom <130936689+Lyriom@users.noreply.github.com>
2023-07-03 20:58:09 -0500

    declaracion de variables
    Se hace una declarion de variables

--- Proyecto.c
+++ Proyecto.c
@@ -3,8 +3,15 @@
 #include <stdlib.h>
 int main(int argc, char const *argv[])
 {
-    /* code hola esto una prueba*/
-    //primer hola mundo
+ //Se declaran variables
+    int opcionesusu;
+    int numero_maquinas;
+    char maquina_n[30];
+    float n_materia_prima;
+    float n_uso_prima;
+    int eliminar_item;
+    int var1;
+    int cantidad;
     printf("Hola Proyecto");
     return 0;
 }
